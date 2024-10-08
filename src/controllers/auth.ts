f5c0b30e6a6a4405a3fdb9650fab63f860d75745
hiigorsilva <higorscontato@gmail.com>
2024-10-08 15:00:15 -0300

    fix: ensure auth file controller validation is correct

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,13 +1,20 @@
 import type { RequestHandler } from 'express'
+import { signupSchema } from '../schemas/signup'
 
 export const signup: RequestHandler = async (req, res) => {
   // validar os dados recebidos
-  // validar email
+  const safeData = signupSchema.safeParse(req.body)
+  if (!safeData.success) {
+    res.status(400).json({ error: safeData.error.flatten().fieldErrors })
+    return
+  }
+
+  // validar emaily
   // verificar username
   // gerar hash de senha
   // cria o user
   // cria  token
   // retorna o resultado (token, user)
 
-  res.json({ message: 'Hello World' })
+  res.json({})
 }
