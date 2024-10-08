c031cf34d808668d344c5300a5a7326952e0592b
hiigorsilva <higorscontato@gmail.com>
2024-10-08 11:35:46 -0300

    feat: create user routes file

---
+++ src/routes/user.ts
@@ -0,0 +1,8 @@
+import { mainRouter } from './main'
+
+// mainRouter.get('/:username')
+// mainRouter.get('/:username/tweets')
+// mainRouter.post('/:username/follow')
+// mainRouter.put('/user')
+// mainRouter.put('/user/avatar')
+// mainRouter.put('/user/cover')
